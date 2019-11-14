08985514a1fd8dee33ef5b2d10b4d1b5166ac36b
madru06 <madru06@outlook.com>
2019-11-13 23:45:41 -0600

    Works added

--- js/custom_scripts.js
+++ js/custom_scripts.js
@@ -1,4 +1,19 @@
 
 function redirect1(){
-    window.location="https://madru06.github.io/madru06.flordeloto/"
+    window.open('https://madru06.github.io/madru06.flordeloto/', '_blank');
+}
+function redirect2(){
+    window.open('https://madru06.github.io/madru06.streetsburguers/', '_blank');
+}
+function redirect3(){
+    window.open('https://madru06.github.io/madru06.OnTV/', '_blank');
+}
+function redirect4(){
+    window.open('https://github.com/madru06/demo-ng-api', '_blank');
+}
+function redirect5(){
+    window.open('https://github.com/madru06/Vue-Demos', '_blank');
+}
+function redirect6(){
+    window.open('https://github.com/madru06/mesero-app', '_blank');
 }
